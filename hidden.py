da35cc3836cca199db0487511c0a5e6873c1a414
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-07-26 19:28:54 +0200

    fixed a security leak

--- hidden.py
+++ hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "A2FaVa2VuytNgYzvCS9LDZFNG",
-            "consumer_secret": "fBXQuJoOwM2lJ0VsJq2124RX43jK9LOTEvxI9HUTLgqVtwZTR3",
-            "token_key": "1123313021013696512-GyJyRAmAE5YQ2OSwZC9qlnRBW2EEZe",
-            "token_secret": "sVyCt1FYjSXenLcu9yfVGM7hkRPc9OmcwGH4LOVRZRoLL"}
+    return {"consumer_key": "A2Fa%%%%%%%%%%%%%%%%%%%%%",
+            "consumer_secret": "fBXQu%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",
+            "token_key": "11233%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",
+            "token_secret": "sVyC%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"}
